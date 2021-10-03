f9fbbbb89300e7a5a11e668be2090ab3bc844cf2
Reshmaja14 <87228757+Reshmaja14@users.noreply.github.com>
2021-10-03 18:16:19 +0530

    Add files via upload

---
+++ HAB-sensor.ino
@@ -0,0 +1,128 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+Adafruit_BME280 bme;
+const int ledPin = 13;
+const int ldrPin = A0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+  ss.begin(GPSBaud);
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+  checkSettings();
+}
+void checkSettings()
+{
+  Serial.println();
+  
+  Serial.print(" * Sleep Mode:            ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+  
+  Serial.print(" * Clock Source:          ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  
+  Serial.print(" * Accelerometer:         ");
+  switch(mpu.getRange())
+  {
+    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+  }  
+
+  Serial.print(" * Accelerometer offsets: ");
+  Serial.print(mpu.getAccelOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getAccelOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getAccelOffsetZ());
+  
+  Serial.println();
+}
+void loop() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println("*C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println("hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println("m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println("%");
+
+  Serial.println();
+  delay(1000);
+  while (ss.available() > 0){
+   gps.encode(ss.read());
+   if (gps.location.isUpdated()){
+     Serial.print("Latitude= "); 
+     Serial.print(gps.location.lat(), 6);
+     Serial.print(" Longitude= "); 
+     Serial.println(gps.location.lng(), 6);
+     Vector rawAccel = mpu.readRawAccel();
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  Serial.print(" Xraw = ");
+  Serial.print(rawAccel.XAxis);
+  Serial.print(" Yraw = ");
+  Serial.print(rawAccel.YAxis);
+  Serial.print(" Zraw = ");
+
+  Serial.println(rawAccel.ZAxis);
+  Serial.print(" Xnorm = ");
+  Serial.print(normAccel.XAxis);
+  Serial.print(" Ynorm = ");
+  Serial.print(normAccel.YAxis);
+  Serial.print(" Znorm = ");
+  Serial.println(normAccel.ZAxis);
+  
+  delay(10);
+  int ldrStatus = analogRead(ldrPin);
+  if (ldrStatus <= 200) {
+  digitalWrite(ledPin, HIGH);
+  Serial.print("Its DARK, Turn on the LED : ");
+  Serial.println(ldrStatus);
+  } else {
+  digitalWrite(ledPin, LOW);
+  Serial.print("Its BRIGHT, Turn off the LED : ");
+  Serial.println(ldrStatus);
+}
+}
